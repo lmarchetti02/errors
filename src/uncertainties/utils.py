e099ad729852b9bd86102c9e21aca3566ac984bd
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-02-18 13:58:38 +0100

    minor changes

--- src/uncertainties/utils.py
+++ src/uncertainties/utils.py
@@ -1,9 +1,19 @@
 from numba import njit
 from typing import NoReturn
+import numpy as np
 
 
 @njit
 def check_sizes(a_list: list) -> NoReturn:
+    """
+    This function checks whether an array is composed
+    of equal-sized arrays.
+
+    Parameters
+    ---
+    a_list: numpy.ndarray
+        The array of arrays.
+    """
     for i, arr in enumerate(a_list):
         for j in range(i, len(a_list)):
             if len(arr) != len(a_list[j]):
