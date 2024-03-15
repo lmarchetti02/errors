59eb282d4f872002996975ea9462ef99e1e66180
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-03-15 12:13:25 +0100

    added functions_single

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -4,4 +4,4 @@ A small Python project for propagating uncertainties fast.
 
 from .utils import check_sizes
 from .basic import products, quotients, sums
-from .advanced import functions
+from .advanced import functions, functions_single
--- src/errors/advanced.py
+++ src/errors/advanced.py
@@ -48,3 +48,46 @@ def functions(var_names: tuple[str], f_expression: str, values: list, errors: li
         res[i] = np.sqrt(((der_values * errors[i]) ** 2).sum())
 
     return res
+
+
+def functions_single(var_names: tuple[str], f_expression: str, values: list, errors: list) -> np.ndarray:
+    """
+    This function propagates the uncertainties in the case of functions
+    of physical quantities, that is, `G = G(A, B, ...)`.
+
+    It is analogous to `functions()`, but it takes single measures
+    instead of arrays of them.
+
+    Parameters
+    ---
+    var_names: tuple[str]
+        The tuple containing the names of the function
+        variables as strings.
+    f_expression: str
+        The string with the expression of the function
+        in the Mathematica syntax.
+    values: list
+        The list containing the values of the function variables.
+    errors: list
+        The list containing the values of the errors on the
+        function variables.
+
+    IMPORTANT: This function considers the function variables as
+               independent, that is, the covariances between said
+               variables are considered 0.
+    """
+
+    variables = [sp.symbols(f"{var_names[i]}") for i in range(len(var_names))]
+    function = parse_mathematica(f_expression)
+
+    derivatives = []
+    for var in variables:
+        derivatives.append(sp.lambdify(variables, sp.diff(function, var)))
+
+    derivatives = np.array(derivatives)
+
+    values = np.array(values)
+    errors = np.array(errors)
+
+    der_values = np.array([der(*values) for der in derivatives])
+    return np.sqrt(((der_values * errors) ** 2).sum())
